2fee6ec1643563f99c9c293238765435118fc106
Mário Salembe <linomario199010@gmail.com>
2026-05-31 23:00:00 +0100

    feat: add ColorBends component with customizable visual effects
    - Introduced a new ColorBends component that utilizes Three.js for rendering dynamic color effects.
    - Added GLSL shaders for advanced visual manipulation based on user-defined parameters.
    - Created utility functions for class name management using clsx and tailwind-merge.
    - Added SVG logo assets in black, white, and simple styles for branding purposes.
    - Configured Tailwind CSS and component structure in components.json for consistent styling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,98 @@
+import {
+  RiGithubFill,
+  RiMailSendFill,
+  RiStackFill,
+  RiStickyNoteAddFill,
+} from "@remixicon/react";
 import Image from "next/image";
+import Link from "next/link";
+
+const navigation = [
+  { name: "Eventos", href: "/events" },
+  { name: "Contacto", href: "/contact" },
+  { name: "Contribuir", href: "/contribute" },
+];
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div>
+      <header>
+        <div className="w-full ">
+          <nav className="max-w-7xl border-b border-gray-100 py-5 mx-auto flex items-center justify-between">
+            <div>
+              <Link href={"/"} className="flex items-center gap-2">
+                <Image
+                  src={"img-logo/simple-logo.svg"}
+                  alt={"Logo"}
+                  width={100}
+                  className="w-5 mt-1"
+                  height={100}
+                />
+                <p className=" text-3xl">annita</p>
+              </Link>
+            </div>
+            <div className="flex items-center gap-10">
+              <div className="flex items-center gap-16">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    className="text-[15px] font-normal text-zinc-900 hover:text-gray-900"
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+              <div className="flex items-center gap-2">
+                <button className="text-base border-gray-200 border rounded-lg px-3 py-1.5 font-normal text-zinc-900 hover:text-gray-900 flex items-center gap-2 font-mono">
+                  1.2K
+                  <RiGithubFill className="size-5 text-gray-800" />
+                </button>
+                <button className="text-base transition-all hover:opacity-75 text-white bg-design-2 border-design-2 border rounded-lg px-3 py-1.5 font-normal  flex items-center gap-2 ">
+                  <RiMailSendFill className="size-4" />
+                  Newsletter
+                </button>
+              </div>
+            </div>
+          </nav>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+
+        <section className="grid max-w-7xl mt-16 mx-auto grid-cols-[60%_40%]">
+          <div className="max-w-3xl">
+            <p className="px-4 py-2 rounded-full text-sm font-normal bg-design-2/10 gap-2 items-center gap- text-design-3 inline-flex mb-6">
+              <RiMailSendFill className="size-4" />
+              Subscreva a nossa newsletter
+            </p>
+            <h1 className="text-8xl leading-16 font-medium">
+              O teu próximo{" "}
+              <span className="text-design-2 font-medium">evento</span> começa
+              aqui.
+            </h1>
+            <p className="mt-10 text-lg font-normal max-w-xl text-zinc-600">
+              Publique ou encontre eventos relacionados com a tecnologia em
+              Angola. Desde meetups a conferências, tudo o que precisas para
+              estares a par do que se passa no mundo tech.
+            </p>
+            <div className="flex items-center gap-2 mt-6">
+              <button className="text-base transition-all hover:opacity-75 text-white bg-design-2 border-design-2 border rounded-lg px-3 py-1.5 font-normal  flex items-center gap-2 ">
+                <RiStackFill className="size-4" />
+                Explorar eventos
+              </button>
+              <button className="text-base transition-all hover:opacity-75 bg-white text-design-3 border-gray-200 border rounded-lg px-3 py-1.5 font-normal  flex items-center gap-2 ">
+                <RiStickyNoteAddFill className="size-4" />
+                Publicar evento
+              </button>
+            </div>
+          </div>
+          <div className="relative">
+            <div className="flex items-center  relative justify-center">
+              <div className="h-96  shadow-2xl z-20 bg-white absolute top-0 border border-gray-100 rounded-3xl w-72" />
+              <div className="h-96 rotate-12 bg-white border absolute top-0 border-gray-100 rounded-3xl w-72" />
+              <div className="h-96 -rotate-12 bg-white border absolute top-0 border-gray-100 rounded-3xl w-72" />
+            </div>
+          </div>
+        </section>
+      </header>
     </div>
   );
 }
